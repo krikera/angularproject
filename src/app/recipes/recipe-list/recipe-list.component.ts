5a0c048f0ccbfeca17fa8f8963f16aaaf7fa694d
krikera <prafulrai522@gmail.com>
2023-11-11 13:21:14 +0530

    More code written day 2

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes:Recipe[] = [
+    new Recipe('A test Recipe','This is simply a test','https://www.whiskaffair.com/wp-content/uploads/2021/10/Paneer-Lababdar-2-1.jpg'),
+    
+  ];
 
 }
